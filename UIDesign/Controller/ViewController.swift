76cfc1a3b94a98d22deb774b9c84d0fe6f7eb04d
Daniyal Khan <dk720820@gmail.com>
2021-12-17 17:21:29 +0500

    comments added to empty viewcontroller

--- UIDesign/Controller/ViewController.swift
+++ UIDesign/Controller/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Connect it with any of the view and manage the UI from here
     }
 
 
